4069fbb31cc4db185b79ec4f5d99a81de1a94362
Siddharth Pal <sidd260504@gmail.com>
2025-07-12 16:24:43 +0530

    change url.js to direct to port 5000

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,2 @@
-export const URL = "http://13.235.67.37";
-export const IF = "http://13.235.67.37/images/";
-
+export const URL = "http://13.235.67.37:5000";
+export const IF = "http://13.235.67.37:5000/images/";
